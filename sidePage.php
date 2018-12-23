fd4976c552032216ad1583d0723c5a9fa537ac66
Benn King <bennkingy@gmail.com>
2018-12-23 21:34:59 +0000

    Added content page styling

--- sidePage.php
+++ sidePage.php
@@ -14,14 +14,14 @@ get_header(); ?>
 
         <div class="container">
             <div class="row">
-                <div class="col-sm-8" style="background-color:green;">
+                <div class="col-sm-8 mainContent">
                     <?php if (have_posts()) : while (have_posts()) : the_post();
                         the_content();
                     endwhile; else: ?>
                         <p>Sorry, no posts matched your criteria.</p>
                     <?php endif; ?>
                 </div>
-                <div class="col-sm-4" style="background-color:pink;">
+                <div class="col-sm-4">
                     <?php get_sidebar(); ?>
                 </div>
             </div>
